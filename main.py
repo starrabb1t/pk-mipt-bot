8c11a8d0c1d89f3f9cd2e20d8a4b6b44a2652c93
starrabb1t <berzin11295@mail.ru>
2020-02-29 18:30:43 +0300

    minor fixes

--- main.py
+++ main.py
@@ -10,23 +10,35 @@ from telegram.ext import Updater, run_async, CommandHandler, ConversationHandler
 
 @run_async
 def start(update, context):
-    help(update,context)
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="Здравствуйте!\n" + \
+                                  "Меня зовут Петр, я бот приемной комиссии НИУ МФТИ. Здесь можно найти ответы на интересующие " + \
+                                  "Вас *вопросы* о поступлении, введя команду /info.\n" + \
+                                  "Просмотреть и подписаться на ближайшие важные *события* можно в календаре с помощью команды /calendar.\n" + \
+                                  "Также же можете просто меня *спросить* напрямую, а я попытаюсь ответить :) ",
+                             parse_mode=ParseMode.MARKDOWN)
 
 @run_async
 def info(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Info menu!")
+    context.bot.send_message(chat_id=update.message.chat_id, text="<info menu>")
 
 @run_async
 def help(update, context):
     context.bot.send_message(chat_id=update.message.chat_id,
-                             text="Здравствуйте!\n" + \
-                             "Меня зовут Петр, я бот приемной комиссии НИУ МФТИ. Здесь можно найти ответы на интересующие " + \
-                             "Вас вопросы о поступлении, введя команду /info.\n" + \
-                             "Также же можете просто меня спросить напрямую, я попытаюсь ответить :) ")
+                             text="1. Ответы на часто задаваемые *вопросы* можно посмотреть, введя команду /info.\n" + \
+                             "2. Просмотреть и подписаться на ближайшие важные *события* можно в календаре с помощью команды /calendar.\n" + \
+                             "3. Также же можете просто меня *спросить* напрямую, я попытаюсь ответить :) ",
+                             parse_mode=ParseMode.MARKDOWN)
 
 @run_async
-def custom_question(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Kaboom!")
+def calendar(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="*bold* _italic_ `fixed width font` [линк-хуинк](http://google.com).",
+                             parse_mode=ParseMode.MARKDOWN)
+
+@run_async
+def custom_text_question(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Хуй соси")
 
 if __name__ == '__main__':
 
@@ -40,6 +52,8 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_handler(CommandHandler('info', info))
+    updater.dispatcher.add_handler(CommandHandler('calendar', calendar))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, custom_text_question))
 
     updater.start_polling()
     updater.idle()
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop mr-clappy-bot
-docker rm mr-clappy-bot
+docker stop pk-mipt-bot
+docker rm pk-mipt-bot
 
 export TELEGRAM_API_TOKEN=871197060:AAHw2FiSrom_d3KD0Ob23kClbXLyaZKskVQ
 
 docker run -d \
 -e TELEGRAM_API_TOKEN \
 -v $(pwd)/data:/opt/app/data \
---name mr-clappy-bot mr-clappy-bot:latest
+--name pk-mipt-bot pk-mipt-bot:latest
