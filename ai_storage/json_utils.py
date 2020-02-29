dfcfe5ed92b9fa1fcaed45dafbe7550b16a917a9
knikita <kakurnikovnikita@gmail.com>
2020-02-29 17:43:39 +0300

    commit flush

--- ai_storage/json_utils.py
+++ ai_storage/json_utils.py
@@ -3,13 +3,13 @@ from ai_storage.utils import assert_filepath
 
 
 def json_dump(json_filepath, json_obj, indent=4):
-    with open(json_filepath, 'w') as f:
+    with open(json_filepath, 'w', encoding='utf8') as f:
         ser = json.dumps(json_obj, indent=indent)
         f.write(ser)
 
 
 def json_load(json_filepath):
     assert_filepath(json_filepath)
-    with open(json_filepath) as f:
+    with open(json_filepath, encoding='utf8') as f:
         json_obj = json.load(f)
     return json_obj
--- ai_storage/parse_data.py
+++
@@ -1,7 +0,0 @@
-import os
-from ai_storage.json_utils import *
-
-
-ai_storage_path = os.path.dirname(__file__)
-
-data_path = os.path.join
