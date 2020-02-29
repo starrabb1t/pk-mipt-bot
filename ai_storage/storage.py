443f9b31c6511d107e4acb79c913d525dbd9edff
knikita <kakurnikovnikita@gmail.com>
2020-02-29 21:26:12 +0300

    refact

--- ai_storage/storage.py
+++ ai_storage/storage.py
@@ -13,20 +13,20 @@ class Storage:
 
     VERSION = '0.1'
 
-    KEY_DATA = 'data'
-    KEY_INFO = 'info'
+    __KEY_DATA = 'data'
+    __KEY_INFO = 'info'
 
     def __init__(self, data_json_path: str, model_bin_path: str):
         json_dirname = os.path.dirname(data_json_path)
-        self.questions_vectors_filepath = os.path.join(json_dirname, self.questions_vectors_filename)
+        self.__questions_vectors_filepath = os.path.join(json_dirname, self.__questions_vectors_filename)
 
-        self.data = json_load(data_json_path)[self.KEY_DATA]
+        self.data = json_load(data_json_path)[self.__KEY_DATA]
         assert isinstance(self.data, dict)
         self.questions = list(self.data.keys())
         assert all((isinstance(x, str) for x in self.questions))
 
-        self.model = gensim.models.KeyedVectors.load_word2vec_format(model_bin_path, binary=True, encoding='utf-8')
-        self.mystem = Mystem()
+        self.__model = gensim.models.KeyedVectors.load_word2vec_format(model_bin_path, binary=True, encoding='utf-8')
+        self.__mystem = Mystem()
 
         self.__load_questions_vectors()
 
@@ -47,16 +47,16 @@ class Storage:
 
     def preprocess(self, question):
         # TODO fix fused words
-        analyzis = self.mystem.analyze(question)
+        analyzis = self.__mystem.analyze(question)
         words = []
         for x in analyzis:
             try:
-                w = x[self.mystem_key_analysis][0][self.mystem_key_analysis_key_lex]
-                p = x[self.mystem_key_analysis][0][self.mystem_key_analysis_key_gr]
+                w = x[self.__mystem_key_analysis][0][self.__mystem_key_analysis_key_lex]
+                p = x[self.__mystem_key_analysis][0][self.__mystem_key_analysis_key_gr]
                 p = p.split(',')[0]
                 p = p.split('=')[0]
-                if p in self.posmap.keys():
-                    p = self.posmap[p]
+                if p in self.__posmap.keys():
+                    p = self.__posmap[p]
                     w += '_'
                     w += p
                     words.append(w)
@@ -65,26 +65,26 @@ class Storage:
 
         return words
 
-    posmap = {
+    __posmap = {
         'S': 'NOUN',
         'V': 'VERB',
         'A': 'ADJ',
         'ADV': 'ADV'
     }
 
-    mystem_key_text = 'text'
-    mystem_key_analysis = 'analysis'
-    mystem_key_analysis_key_lex = 'lex'
-    mystem_key_analysis_key_gr = 'gr'
+    __mystem_key_text = 'text'
+    __mystem_key_analysis = 'analysis'
+    __mystem_key_analysis_key_lex = 'lex'
+    __mystem_key_analysis_key_gr = 'gr'
 
-    questions_vectors_filename = 'questions_vectors.p'
+    __questions_vectors_filename = 'questions_vectors.p'
 
     def get_vectors(self, sentence: str):
         words = self.preprocess(sentence)
         vectors = []
         for x in words:
             try:
-                vector = self.model.get_vector(x)
+                vector = self.__model.get_vector(x)
                 vectors.append(vector)
             except KeyError:
                 pass
@@ -93,9 +93,8 @@ class Storage:
 
     def __load_questions_vectors(self):
         calculate_vectors = True
-        if os.path.isfile(self.questions_vectors_filepath):
-            # self.questions_vectors = np.load(self.questions_vectors_filepath, allow_pickle=True)
-            self.questions_vectors = pickle.load(open(self.questions_vectors_filepath, "rb"))
+        if os.path.isfile(self.__questions_vectors_filepath):
+            self.questions_vectors = pickle.load(open(self.__questions_vectors_filepath, "rb"))
             # TODO assert
             calculate_vectors = False
 
@@ -104,8 +103,7 @@ class Storage:
             for question in self.questions:
                 vectors = self.get_vectors(question)
                 self.questions_vectors[question] = vectors
-            # np.save(self.questions_vectors_filepath, self.questions_vectors)
-            pickle.dump(self.questions_vectors, open(self.questions_vectors_filepath, "wb"))
+            pickle.dump(self.questions_vectors, open(self.__questions_vectors_filepath, "wb"))
 
         for k, v in self.questions_vectors.items():
             self.questions_vectors[k] = np.sum(v, axis=0)
