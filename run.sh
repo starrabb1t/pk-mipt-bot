06fffb58f9cb54b7800186e8bad867fc9a2bb88b
星うさぎ <berzin@clapeyronrobotics.com>
2020-03-01 17:23:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 docker stop pk-mipt-bot
 docker rm pk-mipt-bot
 
-export TELEGRAM_API_TOKEN=1136290332:AAH6bzEYS-i0vN6zRJXHyL6z7Ifiq78UWvk
-
 docker run -d \
 -e TELEGRAM_API_TOKEN \
 -v $(pwd)/data:/opt/app/data \
